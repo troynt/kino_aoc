a804b525aef7a20c4547df0f28c0f421b6b16ae2
troynt <troynt@gmail.com>
2025-12-06 10:13:57 -0500

    rename

--- lib/kino_aoc.ex
+++ lib/kino_aoc.ex
@@ -41,7 +41,7 @@ defmodule KinoAOC do
 
     case result do
       {:ok, {{_version, 200, _reason_phrase}, _headers, body}} -> String.trim_trailing(List.to_string(body), "\n")
-      {:ok, {{_version, status, _reason_phrase}, _headers, body }} -> raise Integer.to_string(status) <> " " <> List.to_string(reason_phrase) <> ": " <> List.to_string(body)
+      {:ok, {{_version, status, reason_phrase}, _headers, body }} -> raise Integer.to_string(status) <> " " <> List.to_string(reason_phrase) <> ": " <> List.to_string(body)
     end
   end
 end
